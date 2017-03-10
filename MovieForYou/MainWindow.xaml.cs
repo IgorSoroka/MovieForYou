520143a6ff93b9ff3c31fe03fa90f1973264a908
Igor Soroka <soroka-igor@mail.ru>
2017-03-10 12:54:37 +0200

    test commit

--- MovieForYou/MainWindow.xaml.cs
+++ MovieForYou/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.TMDb;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,9 +24,31 @@ namespace MovieForYou
     /// </summary>
     public partial class MainWindow : Window
     {
+        ServiceClient first = new ServiceClient("fa314d1331397149188e07fbec92930d");
+
+        List<string> firstcoll = new List<string>();
+
+        CancellationToken token = new CancellationToken();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var movies = await first.Movies.GetUpcomingAsync(null, 1, token);
+
+            foreach (Movie m in movies.Results)
+            {
+                var movie = await first.Movies.GetAsync(m.Id, null, true, token);
+
+                firstcoll.Add(movie.Title);
+            }
+
+            lbMain.ItemsSource = firstcoll;
+            //проверка коммита
         }
     }
 }
