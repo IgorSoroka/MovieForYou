44902cc55f6c0755f6ab126fad220872cfdf18d1
Igor Soroka <soroka-igor@mail.ru>
2017-03-12 21:24:44 +0200

    12-03

--- MovieForYou.BL/Class1.cs
+++ MovieForYou.BL/Class1.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MovieForYou.DAL
+namespace MovieForYou.BL
 {
     public class Class1
     {
-
     }
 }
---
+++ MovieForYou.DAL/DataMovie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.TMDb;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieForYou.DAL
+{
+    public class DataMovie
+    {        
+        public bool Adult { get; }       
+        public AlternativeTitles AlternativeTitles { get; }       
+        public string Backdrop { get; }       
+        public Collection BelongsTo { get; }     
+        public int Budget { get; }       
+        public IEnumerable<Company> Companies { get; }    
+        public IEnumerable<Country> Countries { get; }
+        public MediaCredits Credits { get; }      
+        public ExternalIds External { get; }       
+        public IEnumerable<Genre> Genres { get; }   
+        public string HomePage { get; }
+        public Images Images { get; }
+        public string Imdb { get; }
+        public Keywords Keywords { get; }
+        public IEnumerable<Language> Languages { get; }
+        public string OriginalTitle { get; }
+        public string Overview { get; }
+        public decimal Popularity { get; }  
+        public string Poster { get; }
+        public DateTime? ReleaseDate { get; }
+        public Releases Releases { get; }
+        public long Revenue { get; }
+        public int? Runtime { get; }
+        public string Status { get; }
+        public string TagLine { get; } 
+        public string Title { get; }
+        public Translations Translations { get; }
+        public Videos Videos { get; }
+        public decimal VoteAverage { get; }
+        public int VoteCount { get; }
+    }
+}
